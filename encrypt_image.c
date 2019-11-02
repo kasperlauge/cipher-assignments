985f50224e8ab775fd17664903a80529987dfdb3
kasperlauge <kasperlaugemadsen@gmail.com>
2019-11-02 14:54:13 +0100

    Added oaes reference

--- encrypt_image.c
+++ encrypt_image.c
@@ -12,7 +12,8 @@
 #include "ppm.h"
 
 /* Helper prototypes */
-// Lots of which is inspired from https://github.com/jhjin/OpenAES/blob/master/src/oaes_lib.c
+// Lots of which is heavily inspired from https://github.com/jhjin/OpenAES/blob/master/src/oaes_lib.c
+// Methods taken directly from oaes is named accordingly
 int modInverse(int a, int m);
 void gmix_column(unsigned char *r);
 void inv_gmix_column(unsigned char *r);
@@ -759,32 +760,13 @@ void inv_shift_rows(unsigned char *block) {
 // From https://en.wikipedia.org/wiki/Rijndael_MixColumns
 void gmix_column(unsigned char *r) {
 	unsigned int block_size = 16;
-    // unsigned char a[4];
-    // unsigned char b[4];
-    // unsigned char c;
-    // unsigned char h;
-    // /* The array 'a' is simply a copy of the input array 'r'
-    //  * The array 'b' is each element of the array 'a' multiplied by 2
-    //  * in Rijndael's Galois field
-    //  * a[n] ^ b[n] is element n multiplied by 3 in Rijndael's Galois field */ 
-    // for (c = 0; c < 4; c++) {
-    //     a[c] = r[c];
-    //     /* h is 0xff if the high bit of r[c] is set, 0 otherwise */
-    //     h = (unsigned char)((signed char)r[c] >> 7); /* arithmetic right shift, thus shifting in either zeros or ones */
-    //     b[c] = r[c] << 1; /* implicitly removes high bit because b[c] is an 8-bit char, so we xor by 0x1b and not 0x11b in the next line */
-    //     b[c] ^= 0x1B & h; /* Rijndael's Galois field */
-    // }
-    // r[0] = b[0] ^ a[3] ^ a[2] ^ b[1] ^ a[1]; /* 2 * a0 + a3 + a2 + 3 * a1 */
-    // r[1] = b[1] ^ a[0] ^ a[3] ^ b[2] ^ a[2]; /* 2 * a1 + a0 + a3 + 3 * a2 */
-    // r[2] = b[2] ^ a[1] ^ a[0] ^ b[3] ^ a[3]; /* 2 * a2 + a1 + a0 + 3 * a3 */
-    // r[3] = b[3] ^ a[2] ^ a[1] ^ b[0] ^ a[0]; /* 2 * a3 + a2 + a1 + 3 * a0 */
-		int8_t _temp[4];
-  
-  	_temp[0] = oaes_gf_mul(r[0], 0x02) ^ oaes_gf_mul( r[1], 0x03 ) ^ r[2] ^ r[3];
-  	_temp[1] = r[0] ^ oaes_gf_mul( r[1], 0x02 ) ^ oaes_gf_mul( r[2], 0x03 ) ^ r[3];
-  	_temp[2] = r[0] ^ r[1] ^ oaes_gf_mul( r[2], 0x02 ) ^ oaes_gf_mul( r[3], 0x03 );
-  	_temp[3] = oaes_gf_mul( r[0], 0x03 ) ^ r[1] ^ r[2] ^ oaes_gf_mul( r[3], 0x02 );
-  	memcpy( r, _temp, 4 );
+	int8_t _temp[4];
+
+  _temp[0] = oaes_gf_mul(r[0], 0x02) ^ oaes_gf_mul( r[1], 0x03 ) ^ r[2] ^ r[3];
+  _temp[1] = r[0] ^ oaes_gf_mul( r[1], 0x02 ) ^ oaes_gf_mul( r[2], 0x03 ) ^ r[3];
+  _temp[2] = r[0] ^ r[1] ^ oaes_gf_mul( r[2], 0x02 ) ^ oaes_gf_mul( r[3], 0x03 );
+  _temp[3] = oaes_gf_mul( r[0], 0x03 ) ^ r[1] ^ r[2] ^ oaes_gf_mul( r[3], 0x02 );
+  memcpy( r, _temp, 4 );
 }
 
 void inv_gmix_column(unsigned char *r) {
